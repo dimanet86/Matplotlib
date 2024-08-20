c4151edb143b7cf189a62317cf4c0b2df83b3687
Dima Reshetnikov <dima.reshetnikov@gmail.com>
2024-08-20 14:22:47 +0300

    Fourth task fininshed

--- plt.py
+++ plt.py
@@ -60,4 +60,57 @@ plt.show()
 Task 3: Room Type vs. Availability
 Plot: Create a grouped bar plot to show the average availability_365 for each room_type across the neighborhoods.
 """
+# NB: I preferred neighbourhoods_grops to neighbourhoods because of amount of neighbourhoods (it's large)
+room_types = tuple(data['room_type'].unique())
+neighbourhood_groups = tuple(data['neighbourhood_group'].unique())
 
+averages = {}
+std_devs = {}
+for room_type in room_types:
+    grouped_data = data[data['room_type'] == room_type].groupby('neighbourhood_group')['availability_365']
+    averages[room_type] = grouped_data.mean()
+    std_devs[room_type] = grouped_data.std()  # Calculate standard deviation
+
+x = np.arange(len(neighbourhood_groups))  # the label locations
+width = 0.25  # the width of the bars
+multiplier = 0
+
+fig, ax = plt.subplots(layout='constrained')
+
+for attribute in room_types:
+    offset = width * multiplier
+    rects = ax.bar(x + offset, averages[attribute], width, label=attribute, yerr=std_devs[attribute], capsize=5)
+    ax.bar_label(rects, padding=3)
+    multiplier += 1
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('Days')
+ax.set_title('Availability by neighbourhood groups')
+ax.set_xticks(x + width, neighbourhood_groups)
+ax.legend(loc='upper left', ncols=3)
+ax.set_ylim(0, 250)
+
+plt.show()
+
+"""
+4. Correlation Between Price and Number of Reviews
+Plot: Develop a scatter plot with price on the x-axis and number_of_reviews on the y-axis.
+"""
+# wo  regression line - sorry, had not enough time
+fig, ax = plt.subplots()
+for room_type, color in zip(room_types, ['tab:blue', 'tab:orange', 'tab:green']):
+    x, y = data[data['room_type'] == room_type][['price', 'reviews_per_month']].T.values
+    ax.scatter(x, y, c=color, label=room_type,
+               alpha=0.5, edgecolors='none')
+
+ax.legend()
+ax.grid(True)
+ax.set_xlabel('Price')
+ax.set_ylabel('Number of reviews')
+ax.set_title('Correlation between Price and Number of Reviews')
+plt.show()
+
+"""
+5. Time Series Analysis of Reviews
+Plot: Create a line plot to show the trend of number_of_reviews over time (last_review) for each neighbourhood_group.
+"""
